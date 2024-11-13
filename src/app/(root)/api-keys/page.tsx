f6254c79f0f651a4aa22d6e817253c8f3c5bf396
devMukulSingh <mukulsingh2276@gmail.com>
2024-11-13 18:03:07 +0530

    checking cache in prod

--- src/app/(root)/api-keys/page.tsx
+++ src/app/(root)/api-keys/page.tsx
@@ -13,7 +13,7 @@ const ApiKeysPage = (props: Props) => {
     queryKey: ["bot_token"],
     queryFn: async () => {
       return await fetch(`${base_url_server}/apikeys/get-apikeys`, {
-        cache: "default",
+        cache: "no-cache",
         next: {
           revalidate: 1000,
         },
